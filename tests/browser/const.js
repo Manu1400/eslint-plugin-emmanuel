6be25d1c620c95afd0f9a63ae2e207eee022a45d
Emmanuel Haguet <emmanuel.haguet@laposte.net>
2019-04-10 02:33:24 +0200

    new manual tests

---
+++ tests/browser/const.js
@@ -0,0 +1,14 @@
+const elevation = undefined;
+
+const emailAdress1 = 'prettyandsimple@example.com';
+
+const dt = 444
+const nt = dt === 3 ? 1 : dt + 1;
+
+test = true ? 1 : 2;
+
+var now = new Date()
+now.getYear()
+//console.getYear()
+
+var isFullScreen = document.fullscreen;
--- tests/browser/url.js
+++ tests/browser/url.js
@@ -1,6 +1,8 @@
 // from https://github.com/iamturns/configrrr documentation
 //module.exports = { API_URL: 'http://dev.example.com/' };
 
+module.exports = { API_URL: undefined };
+
 const API_URL = 'http://dev.example.com/'
 var URL = ''
 
@@ -11,6 +13,16 @@ var URL_o = 'http://dev.example.com/test#my_hash'
 { API_URL: 'http://dev.example.com/' }
 
 { API_URL }
+
+var API_TEST;
+
+//TODO : use https://github.com/unshiftio/requires-port
+API_URL = 'http://localhost:80/'
+
+//TODO: use https://github.com/jonschlinkert/is-git-url
+//TODO: avoid IP
+API_URL = 'http://192.168.101.127/user/project.git'
+
 
 // anon function
 
