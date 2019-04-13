8a5cdc09890719702e6975f2ceafd6cb212aafcb
Emmanuel Haguet <emmanuel.haguet@laposte.net>
2019-04-13 08:42:39 +0200

    add cases in manual test files

--- tests/NaN.js
+++ tests/NaN.js
@@ -1,3 +1,21 @@
 NaN = "jj"
 
 undefined = "jj"
+
+// eslint-disable-next-line no-magic-numbers
+var x = 10;
+if (x === x) {
+    x = 20;
+}
+if (Number(x) === Number(x)) { // useless
+    x = 20;
+}
+if (Math.random() === Math.random()) {
+    x = 20;
+}
+// fixed proposal can be:
+var first = Math.random();
+var second = Math.random()
+if (first === second) {
+    x = 20;
+}
