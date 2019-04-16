4892aeb1571c4ce4e3d16c05cba69918a05cdabc
Emmanuel Haguet <emmanuel.haguet@laposte.net>
2019-04-16 16:03:31 +0200

    implement rule no-redefine-require

--- tests/lib/rules/no-redefine-require.js
+++ tests/lib/rules/no-redefine-require.js
@@ -4,43 +4,78 @@
  */
 "use strict";
 
-//------------------------------------------------------------------------------
-// Requirements
-//------------------------------------------------------------------------------
-
-var rule = require("../../../lib/rules/no-redefine-require"),
-
-    RuleTester = require("eslint").RuleTester;
-
-
-//------------------------------------------------------------------------------
-// Tests
-//------------------------------------------------------------------------------
+const rule = require("../../../lib/rules/no-redefine-require")
+const RuleTester = require("eslint").RuleTester;
 
 var ruleTester = new RuleTester();
 ruleTester.run("no-redefine-require", rule, {
-
     valid: [
       {
         code: "var a = require('module');",
       },
       {
         code: "var a = require('./file');",
+      },
+      {
+        code: "var a = require('./file.js');",
+      },
+      {
+        code: "var obj = require('./file.json');",
+      },
+      {
+        code: "var fs = require(\"\");",
+      },
+      {
+        code: "var fs = require();",
       }
     ],
-
     invalid: [
         {
-            code: "var fs = require(\"\");",
+            code: "var require = require(\"\");",
             errors: [{
-                message: "Fill me in.",
+                message: "require redefined",
+                nodeType: 'VariableDeclarator',
             }]
         },
         {
-            code: "var fs = require();",
+            code: "var require = \"\"",
             errors: [{
-                message: "Fill me in.",
+                message: "require redefined",
+                nodeType: 'VariableDeclarator',
             }]
-        }
+        },
+        {
+          code: "var require;",
+          errors: [{
+              message: "require redefined",
+              nodeType: 'VariableDeclarator',
+          }]
+        },
+        {
+          code: "const require = \"\"",
+          parserOptions: { ecmaVersion: 2019 },
+          errors: [{
+              message: "require redefined",
+              nodeType: 'VariableDeclarator',
+          }]
+        },
+        /*
+        {
+          code: "var {require} = test;",
+          parserOptions: { ecmaVersion: 2019 },
+          errors: [{
+              message: "require redefined",
+              nodeType: 'VariableDeclarator',
+          }]
+        },
+        {
+          code: "const {require} = test;",
+          parserOptions: { ecmaVersion: 2019 },
+          errors: [{
+              message: "require redefined",
+              nodeType: 'VariableDeclarator',
+          }]
+        },
+        */
     ]
 });
