3b844a48a7e90dd05f8f1dc12c1a70d22981cf68
Emmanuel Haguet <emmanuel.haguet@laposte.net>
2019-06-11 19:11:20 +0200

    new rule: no-similar-fn-params

---
+++ tests/lib/rules/no-similar-fn-params.js
@@ -0,0 +1,69 @@
+/**
+ * @fileoverview 
+ * @author Emmanuel
+ */
+"use strict";
+
+const rule = require("../../../lib/rules/no-similar-fn-params")
+const RuleTester = require("eslint").RuleTester
+
+var ruleTester = new RuleTester();
+ruleTester.run("no-similar-fn-params", rule, {
+
+    valid: [
+      {
+        code: "function myFunction (isPublic, isMinor) {}",
+      }, {
+        code: "function myFunction (aaa, aab=0) {}",
+        parserOptions: { ecmaVersion: 2019 },
+      },
+    ],
+
+    invalid: [
+        {
+            code: "function myFunction (aaaaaa, aaaaaab) {}",
+            errors: [{
+                message: "Avoid similar params names",
+                type: "FunctionDeclaration"
+            }]
+        }, {
+            code: "function myFunction (isPublicF, isPublicM) {}",
+            errors: [{
+                message: "Avoid similar params names",
+                type: "FunctionDeclaration"
+            }]
+        }, {
+            code: "function myFunction (myFirstArg, isPublicF, isPublicM) {}",
+            errors: [{
+                message: "Avoid similar params names",
+                type: "FunctionDeclaration"
+            }]
+        }, {
+            code: "function myFunction (isPublic, _isPublicF) {}",
+            errors: [{
+                message: "Avoid similar params names",
+                type: "FunctionDeclaration"
+            }]
+        }, {
+            code: "(function (aaa, aab){})",
+            errors: [{
+                message: "Avoid similar params names",
+                type: "FunctionExpression"
+            }]
+        }, {
+            code: "(function (aaa, aabb){})",
+            options: [2],
+            errors: [{
+                message: "Avoid similar params names",
+                type: "FunctionExpression"
+            }]
+        }, {
+            code: "(function (aaa, aabb){})",
+            options: [2, false],
+            errors: [{
+                message: "Avoid similar params names",
+                type: "FunctionExpression"
+            }]
+        },
+    ]
+});
