ca06e2839420a98bf18dd7b3235e9501f8659cbf
Emmanuel Haguet <emmanuel.haguet@laposte.net>
2019-06-20 06:10:52 +0200

    fix a bug on `too-many-comment` rule config

--- tests/lib/rules/too-many-comments.js
+++ tests/lib/rules/too-many-comments.js
@@ -6,6 +6,14 @@ ruleTester.run('too-many-comments', rule, {
   valid: [
     {code: 'var simple = /ab/;'},
     {code: '// a simple comment'},
+    {
+      code: '// a simple comment',
+      options: [1],
+    },
+    {
+      code: 'var simple = /c/;',
+      options: [0],
+    },
   ],
 
   invalid: [
@@ -26,6 +34,36 @@ ruleTester.run('too-many-comments', rule, {
       `,
       errors: [{
         message: 'too many comments in this file with 13 comments',
+        type: "Line"
+      }],
+    }, {
+      code: `// comment one`,
+      options: [0],
+      errors: [{
+        message: 'too many comments in this file with 1 comments',
+      }],
+    }, {
+      code: `/* comment */`,
+      options: [0],
+      errors: [{
+        message: 'too many comments in this file with 1 comments',
+        type: "Block"
+      }],
+    }, {
+      code: `/* comment */`,
+      parser: "flow-parser",
+      options: [0],
+      errors: [{
+        message: 'too many comments in this file with 1 comments',
+        type: "Block"
+      }],
+    }, {
+      code: `// use parser flow-parser`,
+      parser: "flow-parser",
+      options: [0],
+      errors: [{
+        message: 'too many comments in this file with 1 comments',
+        type: "Line"
       }],
     },
   ]
