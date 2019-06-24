936ae5d82628313e31a261e491091159d3f13152
Emmanuel Haguet <emmanuel.haguet@laposte.net>
2019-06-24 19:59:02 +0200

    prefer-flatmap rule: add metadata

--- tests/lib/rules/prefer-flatmap.js
+++ tests/lib/rules/prefer-flatmap.js
@@ -18,7 +18,11 @@ const valid = [
     code: "var arr = [2, 3, 4]; arr.map(function(item){ ; }).flat()",
   }, {
     code: "var arr = [2, 3, 4]; arr.map(duplicate).flat()",
-  },
+  }, { // from https://github.com/sindresorhus/eslint-plugin-unicorn/blob/master/docs/rules/prefer-flat-map.md
+    code: "[1, 2, 3].flatMap(i => [i])",
+  }, /* {
+    code: "[1, 2, 3].map(i => [i]).foo().flat()",
+  }, */
 ]
 const invalid = [
   {
@@ -37,7 +41,7 @@ const invalid = [
   },
 ]
 
-var ruleTester = new RuleTester();
+var ruleTester = new RuleTester({parserOptions: { ecmaVersion: 2019 }});
 ruleTester.run("prefer-flapmap", rule, { valid, invalid });
 
 module.exports = {
